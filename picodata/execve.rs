418c2f225e2d2ba8685b75be4747b18df3cf7c38
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2021-12-03 15:55:32 +0300

    Fix execve::which operation
    It used to panic if the env variable wasn't set.
    Also, this patch covers it with tests.

--- picodata/execve.rs
+++ picodata/execve.rs
@@ -1,6 +1,6 @@
 use libc::c_char;
-use std::path;
 use std::ffi::CString;
+use std::path;
 
 #[must_use]
 pub fn execve(argv: Vec<CString>, envp: Vec<CString>) -> errno::Errno {
@@ -25,7 +25,8 @@ pub fn execve(argv: Vec<CString>, envp: Vec<CString>) -> errno::Errno {
 }
 
 pub fn which(prog: &str) -> Option<path::PathBuf> {
-    for p in std::env::var("PATH").unwrap().split(":") {
+    let path = std::env::var("PATH").unwrap_or_default();
+    for p in path.split(":") {
         let pb: path::PathBuf = [p, prog].iter().collect();
         if pb.as_path().exists() {
             return Some(pb);
@@ -34,3 +35,58 @@ pub fn which(prog: &str) -> Option<path::PathBuf> {
     None
 }
 
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::fs::File;
+    use tempfile::tempdir;
+
+    #[test]
+    fn test_which() -> Result<(), std::io::Error> {
+        let temp = vec![tempdir()?, tempdir()?];
+        File::create(temp[0].path().join("b0"))?;
+        File::create(temp[1].path().join("b1"))?;
+        for p in &temp {
+            File::create(p.path().join("bin"))?;
+        }
+
+        let _savepoint = PathSavepoint::new();
+
+        // which can handle unset variable
+        std::env::remove_var("PATH");
+        assert_eq!(which("void"), None);
+
+        // path order matters
+        let path = format!("{}:{}", temp[0].path().display(), temp[1].path().display());
+        std::env::set_var("PATH", path);
+
+        assert_eq!(which("void"), None);
+        assert_eq!(which("bin"), Some(temp[0].path().join("bin")));
+        assert_eq!(which("b0"), Some(temp[0].path().join("b0")));
+        assert_eq!(which("b1"), Some(temp[1].path().join("b1")));
+
+        std::env::set_var("PATH", "ensure:savepoint:works");
+        drop(_savepoint);
+        assert_ne!(
+            std::env::var("PATH").ok(),
+            Some("ensure:savepoint:works".to_owned())
+        );
+
+        Ok(())
+    }
+
+    struct PathSavepoint(Option<String>);
+    impl PathSavepoint {
+        fn new() -> Self {
+            Self(std::env::var("PATH").ok())
+        }
+    }
+    impl Drop for PathSavepoint {
+        fn drop(&mut self) {
+            match &self.0 {
+                Some(v) => std::env::set_var("PATH", v),
+                None => std::env::remove_var("PATH"),
+            }
+        }
+    }
+}
