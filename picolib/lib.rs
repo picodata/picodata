20ff337b6151a72d1dfb3cc8d5f64399460e8ee7
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2021-12-13 13:21:13 +0300

    Implement simplest raft node
    - There's a fiber which ticks logical clock.
    - There's a `raft_propose` API exposed in the Tarantool console.
    
    Close https://gitlab.com/picodata/picodata/picodata/-/issues/7
    Close https://gitlab.com/picodata/picodata/picodata/-/issues/8

--- picolib/lib.rs
+++ picolib/lib.rs
@@ -1,13 +1,37 @@
 use slog::{debug, info, o};
 use std::os::raw::c_int;
 use ::tarantool::hlua;
+use std::time::Duration;
 mod tarantool;
+use ::tarantool::fiber;
 
 pub struct InnerTest {
     pub name: &'static str,
     pub body: fn(),
 }
 inventory::collect!(InnerTest);
+
+use std::cell::RefCell;
+use std::cell::RefMut;
+use std::cell::Ref;
+use std::rc::Rc;
+
+use raft::prelude::*;
+
+#[derive(Default)]
+struct Stash {
+    raft_node: Option<RawNode<MemStorage>>,
+    raft_loop: Option<fiber::LuaUnitJoinHandle>,
+}
+
+impl std::fmt::Debug for Stash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("")
+            .field("raft_node", &self.raft_node.is_some())
+            .field("raft_loop", &self.raft_loop.is_some())
+            .finish()
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn luaopen_picolib(l: *mut std::ffi::c_void) -> c_int {
@@ -21,22 +45,38 @@ pub extern "C" fn luaopen_picolib(l: *mut std::ffi::c_void) -> c_int {
     match command.as_deref() {
         Ok("run") => {
             main_run();
-        }
-        Ok(_) => {}
-        Err(_) => {}
-    };
+        },
+        Ok(_) => {},
+        Err(_) => {},
+    }
 
     unsafe {
         let l = hlua::Lua::from_existing_state(l, false);
-
-        let mut test = Vec::new();
-        for t in inventory::iter::<InnerTest> {
-            test.push((t.name, hlua::function0(t.body)));
-        }
-
         let luamod: hlua::LuaTable<_> = (&l).push(vec![()]).read().unwrap();
         luamod.set("VERSION", env!("CARGO_PKG_VERSION"));
-        luamod.set("test", test);
+
+        //
+        // Export inner tests
+        {
+            let mut test = Vec::new();
+            for t in inventory::iter::<InnerTest> {
+                test.push((t.name, hlua::function0(t.body)));
+            }
+            luamod.set("test", test);
+        }
+
+        //
+        // Export public API
+        let stash: Rc<RefCell<Stash>> = Default::default();
+        raft_init(&stash);
+        {
+            let stash = stash.clone();
+            luamod.set("get_stash", hlua::function0(move || {get_stash(&stash)}));
+        }
+        {
+            let stash = stash.clone();
+            luamod.set("raft_propose", hlua::function1(move |x| {raft_propose(&stash, x)}));
+        }
 
         use hlua::AsLua;
         (&l).push(&luamod).forget();
@@ -61,6 +101,7 @@ fn main_run() {
     tarantool::eval(
         r#"
         box.schema.user.grant('guest', 'super', nil, nil, {if_not_exists = true})
+        box.schema.func.create('raft_propose', {language = "C", if_not_exists = true})
         box.cfg({log_level = 6})
     "#,
     );
@@ -81,4 +122,154 @@ fn main_run() {
         tarantool::package(),
         tarantool::version()
     );
+
+    // raft_main();
+}
+
+use raft::eraftpb::ConfState;
+use raft::storage::MemStorage;
+
+fn get_stash(stash: &Rc<RefCell<Stash>>) {
+    let stash: Ref<Stash> = stash.borrow();
+    println!("{:?}", stash);
+}
+
+// A simple example about how to use the Raft library in Rust.
+fn raft_init(stash: &Rc<RefCell<Stash>>) {
+    // Create a storage for Raft, and here we just use a simple memory storage.
+    // You need to build your own persistent storage in your production.
+    // Please check the Storage trait in src/storage.rs to see how to implement one.
+    let storage = MemStorage::new_with_conf_state(ConfState::from((vec![1], vec![])));
+
+    let logger = slog::Logger::root(tarantool::SlogDrain, o!());
+
+    // Create the configuration for the Raft node.
+    let cfg = Config {
+        // The unique ID for the Raft node.
+        id: 1,
+        // Election tick is for how long the follower may campaign again after
+        // it doesn't receive any message from the leader.
+        election_tick: 10,
+        // Heartbeat tick is for how long the leader needs to send
+        // a heartbeat to keep alive.
+        heartbeat_tick: 3,
+        // The max size limits the max size of each appended message. Mostly, 1 MB is enough.
+        max_size_per_msg: 1024 * 1024 * 1024,
+        // Max inflight msgs that the leader sends messages to follower without
+        // receiving ACKs.
+        max_inflight_msgs: 256,
+        // The Raft applied index.
+        // You need to save your applied index when you apply the committed Raft logs.
+        applied: 0,
+        ..Default::default()
+    };
+
+    // Create the Raft node.
+    let r = RawNode::new(&cfg, storage, &logger).unwrap();
+    stash.borrow_mut().raft_node = Some(r);
+
+    let loop_fn = {
+        let stash = stash.clone();
+        move || {
+            let logger = slog::Logger::root(tarantool::SlogDrain, o!());
+            loop {
+                fiber::sleep(Duration::from_millis(100));
+                let mut stash: RefMut<Stash> = stash.borrow_mut();
+                let mut raft_node = stash.raft_node.as_mut().unwrap();
+                raft_node.tick();
+                on_ready(&mut raft_node, &logger);
+            }
+        }
+    };
+
+    stash.borrow_mut().raft_loop = Some(fiber::defer_proc(loop_fn));
+}
+
+fn on_ready(
+    raft_group: &mut RawNode<MemStorage>,
+    logger: &slog::Logger,
+) {
+    if !raft_group.has_ready() {
+        return;
+    }
+    let store = raft_group.raft.raft_log.store.clone();
+
+    // Get the `Ready` with `RawNode::ready` interface.
+    let mut ready: Ready = raft_group.ready();
+    println!("{:?}", ready);
+
+    let handle_messages = |msgs: Vec<Message>| {
+        for _msg in msgs {
+            // Send messages to other peers.
+        }
+    };
+
+    if !ready.messages().is_empty() {
+        // Send out the messages come from the node.
+        handle_messages(ready.take_messages());
+    }
+
+    if !ready.snapshot().is_empty() {
+        // This is a snapshot, we need to apply the snapshot at first.
+        store.wl().apply_snapshot(ready.snapshot().clone()).unwrap();
+    }
+
+    let mut _last_apply_index = 0;
+    let mut handle_committed_entries = |committed_entries: Vec<Entry>| {
+        for entry in committed_entries {
+            // Mostly, you need to save the last apply index to resume applying
+            // after restart. Here we just ignore this because we use a Memory storage.
+            _last_apply_index = entry.index;
+
+            if entry.data.is_empty() {
+                // Emtpy entry, when the peer becomes Leader it will send an empty entry.
+                continue;
+            }
+
+            if entry.get_entry_type() == EntryType::EntryNormal {
+                let key = entry.data.get(0).unwrap();
+                info!(logger, "Inserted key: {}", key);
+                // if let Some(value) = cbs.remove(key) {
+                // }
+            }
+
+            // TODO: handle EntryConfChange
+        }
+    };
+    handle_committed_entries(ready.take_committed_entries());
+
+    if !ready.entries().is_empty() {
+        // Append entries to the Raft log.
+        store.wl().append(ready.entries()).unwrap();
+    }
+
+    if let Some(hs) = ready.hs() {
+        // Raft HardState changed, and we need to persist it.
+        store.wl().set_hardstate(hs.clone());
+    }
+
+    if !ready.persisted_messages().is_empty() {
+        // Send out the persisted messages come from the node.
+        handle_messages(ready.take_persisted_messages());
+    }
+
+    // Advance the Raft.
+    let mut light_rd = raft_group.advance(ready);
+    // Update commit index.
+    if let Some(commit) = light_rd.commit_index() {
+        store.wl().mut_hard_state().set_commit(commit);
+    }
+    // Send out the messages.
+    handle_messages(light_rd.take_messages());
+    // Apply all committed entries.
+    handle_committed_entries(light_rd.take_committed_entries());
+    // Advance the apply index.
+    raft_group.advance_apply();
+}
+
+#[no_mangle]
+fn raft_propose(stash: &Rc<RefCell<Stash>>, key: u8) {
+    let mut stash: RefMut<Stash> = stash.borrow_mut();
+    let raft_node = stash.raft_node.as_mut().unwrap();
+    raft_node.propose(vec![], vec![key]).unwrap();
 }
