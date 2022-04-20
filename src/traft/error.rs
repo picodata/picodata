62084c85daa65f25e4e8a64ae01b692ce5f4ba47
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2022-04-20 10:22:31 +0300

    Address review comments: remove unused error variants

--- src/traft/error.rs
+++ src/traft/error.rs
@@ -1,20 +1,13 @@
 use raft::StorageError;
 use rmp_serde::decode::Error as RmpDecodeError;
-use rmp_serde::encode::Error as RmpEncodeError;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum CoercionError {
     #[error("unknown entry type ({0})")]
     UnknownEntryType(i32),
-    #[error("unknown message type ({0})")]
-    UnknownMessageType(i32),
     #[error("invalid msgpack: {0}")]
     MsgpackDecodeError(#[from] RmpDecodeError),
-    #[error("failed encoding msgpack: {0}")]
-    MsgpackEncodeError(#[from] RmpEncodeError),
-    #[error("invalid base64 string")]
-    InvalidBase64(#[from] base64::DecodeError),
 }
 
 impl From<CoercionError> for StorageError {
@@ -27,6 +20,4 @@ impl From<CoercionError> for StorageError {
 pub enum PoolSendError {
     #[error("unknown recipient")]
     UnknownRecipient,
-    #[error("message coercion")]
-    MessageCoercionError(#[from] CoercionError),
 }
