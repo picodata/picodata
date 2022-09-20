438944db2786b6a4e1487972af0b622461466a67
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2022-09-20 15:04:03 +0300

    test: bypass cargo test args into the inner test

--- test/inner.rs
+++ test/inner.rs
@@ -1,6 +1,7 @@
 fn main() {
     let status = std::process::Command::new(env!("CARGO_BIN_EXE_picodata"))
         .arg("test")
+        .args(std::env::args().skip(1))
         .status()
         .unwrap();
 
