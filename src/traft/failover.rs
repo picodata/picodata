69419a85795a6b6e84212f2b4f3ff5000603df1b
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2022-11-17 12:13:07 +0300

    fix: reimplement graceful shutdown logics

--- src/traft/failover.rs
+++ src/traft/failover.rs
@@ -1,101 +1,128 @@
 use std::time::{Duration, Instant};
 
-use ::tarantool::fiber::sleep;
-use ::tarantool::unwrap_or;
+use ::tarantool::fiber;
 
 use crate::tlog;
-
+use crate::traft;
 use crate::traft::error::Error;
 use crate::traft::event;
 use crate::traft::node;
 use crate::traft::rpc;
+use crate::traft::CurrentGradeVariant;
 use crate::traft::TargetGradeVariant;
 use crate::traft::{UpdatePeerRequest, UpdatePeerResponse};
+use crate::unwrap_ok_or;
 
 pub fn on_shutdown() {
+    // 1. Try setting target grade Offline in a separate fiber
+    tlog!(Info, "trying to shutdown gracefully ...");
+    let go_offline = fiber::Builder::new()
+        .name("go_offline")
+        .func(|| match go_offline() {
+            Ok(()) => tlog!(Info, "target grade set Offline"),
+            Err(e) => tlog!(Error, "failed setting target grade Offline: {e}"),
+        });
+    std::mem::forget(go_offline.start());
+
+    // 2. Meanwhile, wait until either it succeeds or there is no quorum.
     let node = node::global().unwrap();
-    let voters = node
-        .storage
-        .raft
-        .voters()
-        .expect("failed reading voters")
-        .unwrap_or_default();
-    let learners = node
-        .storage
-        .raft
-        .learners()
-        .expect("failed reading learners")
-        .unwrap_or_default();
-    let have_active_learners = learners.into_iter().any(|raft_id| {
-        node.storage
-            .peers
-            .get(&raft_id)
-            // TODO: change to peer.may_respond()
-            .map(|peer| peer.is_online())
-            .unwrap_or(false)
-    });
-    let raft_id = node.status().id;
+    let raft_id = node.raft_id();
+    loop {
+        let me = unwrap_ok_or!(
+            node.storage.peers.get(&raft_id),
+            Err(e) => {
+                tlog!(Error, "{e}");
+                break;
+            }
+        );
 
-    if voters == [raft_id] && !have_active_learners {
-        tlog!(Warning, "the last active instance has shut down");
-        return;
+        if me.current_grade == CurrentGradeVariant::Offline {
+            tlog!(Info, "graceful shutdown succeeded");
+
+            // Dirty hack. Wait a little bit more before actually
+            // shutting down. Raft commit index is a local value. Other
+            // nodes may still be unaware that `me.current_grade` is
+            // commmitted. Give them some more time to communicate.
+            fiber::sleep(Duration::from_millis(100));
+            break;
+        }
+
+        let voters = node
+            .storage
+            .raft
+            .voters()
+            .expect("failed reading voters")
+            .unwrap_or_default();
+
+        let quorum = voters.len() / 2 + 1;
+        let voters_alive = voters
+            .iter()
+            .filter_map(|raft_id| node.storage.peers.get(raft_id).ok())
+            .filter(|peer| peer.current_grade == CurrentGradeVariant::Online)
+            .count();
+
+        if voters_alive < quorum {
+            tlog!(Info, "giving up, there is no quorum");
+            break;
+        }
+
+        let Ok(()) = event::wait(event::Event::TopologyChanged) else { break };
     }
+}
 
-    let peer = node.storage.peers.get(&raft_id).unwrap();
+fn go_offline() -> traft::Result<()> {
+    let node = node::global()?;
+    let raft_id = node.raft_id();
+
+    let peer = node.storage.peers.get(&raft_id)?;
     let cluster_id = node
         .storage
         .raft
-        .cluster_id()
-        .unwrap()
-        .expect("cluster_id must be present");
+        .cluster_id()?
+        .ok_or_else(|| Error::other("missing cluster_id value in storage"))?;
+
     let req = UpdatePeerRequest::new(peer.instance_id, cluster_id)
         .with_target_grade(TargetGradeVariant::Offline);
 
-    // will run until we get successfully deactivate or tarantool shuts down
-    // the on_shutdown fiber (after 3 secs)
     loop {
-        let node = node::global().unwrap();
-        let leader_id = unwrap_or!(node.status().leader_id, {
+        let Some(leader_id) = node.status().leader_id else {
             node.wait_status();
-            continue;
-        });
-        if leader_id == raft_id {
-            if let Err(e) = node.handle_topology_request_and_wait(req.into()) {
-                crate::warn_or_panic!("failed to deactivate myself: {}", e);
-            }
-            break;
-        }
-        let leader = node.storage.peers.get(&leader_id).unwrap();
-        let wait_before_retry = Duration::from_millis(300);
-        let now = Instant::now();
+            continue
+        };
 
-        let res = rpc::net_box_call(&leader.peer_address, &req, Duration::MAX);
-        let res = match res {
+        let now = Instant::now();
+        let wait_before_retry = Duration::from_millis(300);
+
+        if leader_id == raft_id {
+            match node.handle_topology_request_and_wait(req.clone().into()) {
+                Err(Error::NotALeader) => {
+                    // We've lost leadership while waiting for NodeImpl
+                    // mutex. Retry after a small pause.
+                    fiber::sleep(wait_before_retry.saturating_sub(now.elapsed()));
+                    continue;
+                }
+                Err(e) => break Err(e),
+                Ok(_) => break Ok(()),
+            }
+        }
+
+        let leader = node.storage.peers.get(&leader_id)?;
+        let res = match rpc::net_box_call(&leader.peer_address, &req, Duration::MAX) {
             Ok(UpdatePeerResponse::Ok) => Ok(()),
             Ok(UpdatePeerResponse::ErrNotALeader) => Err(Error::NotALeader),
             Err(e) => Err(e.into()),
         };
+
         match res {
-            Ok(()) => {
-                break;
-            }
+            Ok(()) => break Ok(()),
             Err(e) => {
-                tlog!(Warning, "failed to deactivate myself: {e}, retry...";
-                    "peer" => &leader.peer_address,
+                tlog!(Warning,
+                    "failed setting target grade Offline: {e}, retrying ...";
+                    "peer" => %leader,
                 );
-                sleep(wait_before_retry.saturating_sub(now.elapsed()));
+                fiber::sleep(wait_before_retry.saturating_sub(now.elapsed()));
                 continue;
             }
         };
-    }
-
-    // no need to wait for demotion if we weren't a voter
-    if !voters.contains(&raft_id) {
-        return;
-    }
-
-    tlog!(Debug, "waiting for demotion");
-    if let Err(e) = event::wait(event::Event::Demoted) {
-        tlog!(Warning, "failed to wait for self demotion: {e}");
     }
 }
--- test/int/test_shutdown.py
+++ test/int/test_shutdown.py
@@ -3,10 +3,18 @@ import pytest
 import signal
 from conftest import Cluster, Instance
 
+ON_SHUTDOWN_TIMEOUT = "on_shutdown triggers failed"
+
 
 @pytest.fixture
 def cluster2(cluster: Cluster):
     cluster.deploy(instance_count=2)
+    return cluster
+
+
+@pytest.fixture
+def cluster3(cluster: Cluster):
+    cluster.deploy(instance_count=3)
     return cluster
 
 
@@ -32,7 +40,7 @@ def test_gl119_panic_on_shutdown(cluster2: Cluster):
     # it can't win the election because there is no quorum
     i2.assert_raft_status("Candidate")
 
-    crawler = log_crawler(i2, "on_shutdown triggers failed")
+    crawler = log_crawler(i2, ON_SHUTDOWN_TIMEOUT)
 
     # stopping i2 in that state still shouldn't be a problem
     assert i2.terminate() == 0
@@ -41,18 +49,66 @@ def test_gl119_panic_on_shutdown(cluster2: Cluster):
     assert crawler.matched
 
 
-@pytest.mark.xfail
-def test_gl127_graceul_shutdown(cluster2: Cluster):
+def test_single(instance: Instance):
+    crawler = log_crawler(instance, ON_SHUTDOWN_TIMEOUT)
+
+    instance.terminate(kill_after_seconds=1)
+    assert not crawler.matched
+
+
+def test_couple_leader_first(cluster2: Cluster):
     i1, i2 = cluster2.instances
 
     # make sure i1 is leader
-    i1.promote_or_fail()
-    i2.wait_online()
+    i1.assert_raft_status("Leader")
+    i2.assert_raft_status("Follower", leader_id=i1.raft_id)
 
-    crawler = log_crawler(i1, "on_shutdown triggers failed")
+    c1 = log_crawler(i1, ON_SHUTDOWN_TIMEOUT)
+    i1.terminate(kill_after_seconds=1)
+    assert not c1.matched
 
-    # on_shutdown triggers will timeout after 3sec
-    # so we must wait longer than that
-    i1.terminate(kill_after_seconds=10)
+    i2.assert_raft_status("Leader")
+    i1_info = i2.call("picolib.peer_info", i1.instance_id)
+    assert i1_info["target_grade"]["variant"] == "Offline"
+    assert i1_info["current_grade"]["variant"] == "Offline"
 
-    assert not crawler.matched
+    c2 = log_crawler(i2, ON_SHUTDOWN_TIMEOUT)
+    i2.terminate(kill_after_seconds=1)
+    assert not c2.matched
+
+
+def test_couple_follower_first(cluster2: Cluster):
+    i1, i2 = cluster2.instances
+
+    # make sure i1 is leader
+    i1.assert_raft_status("Leader")
+    i2.assert_raft_status("Follower", leader_id=i1.raft_id)
+
+    c2 = log_crawler(i2, ON_SHUTDOWN_TIMEOUT)
+    i2.terminate(kill_after_seconds=1)
+    assert not c2.matched
+
+    i2_info = i1.call("picolib.peer_info", i2.instance_id)
+    assert i2_info["target_grade"]["variant"] == "Offline"
+    assert i2_info["current_grade"]["variant"] == "Offline"
+
+    c1 = log_crawler(i1, ON_SHUTDOWN_TIMEOUT)
+    i1.terminate(kill_after_seconds=1)
+    assert not c1.matched
+
+
+def test_threesome(cluster3: Cluster):
+    i1, i2, i3 = cluster3.instances
+
+    # make sure i1 is leader
+    i1.assert_raft_status("Leader")
+    i2.assert_raft_status("Follower", leader_id=i1.raft_id)
+    i3.assert_raft_status("Follower", leader_id=i1.raft_id)
+
+    c1 = log_crawler(i1, ON_SHUTDOWN_TIMEOUT)
+    i1.terminate(kill_after_seconds=1)
+    assert not c1.matched
+
+    c2 = log_crawler(i2, ON_SHUTDOWN_TIMEOUT)
+    i2.terminate(kill_after_seconds=1)
+    assert not c2.matched
