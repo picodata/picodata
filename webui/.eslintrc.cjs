91ba25307f9ea7647e97641cb558bab228133f24
Egor Fuchezhi <e.fuchezhi@picodata.io>
2023-03-31 12:46:46 +0300

    Added vite plugin for custom build

--- webui/.eslintrc.cjs
+++ webui/.eslintrc.cjs
@@ -1,97 +1,94 @@
 module.exports = {
-    extends: [
-        'airbnb-typescript',
-        'airbnb/hooks',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/jsx-runtime',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
-        'prettier',
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier', 'import'],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  ignorePatterns: ['.eslintrc.cjs, vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: ['./tsconfig.eslint.json'],
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/no-unescaped-entities': [
+      'error',
+      {
+        forbid: ['>', '"', '}'],
+      },
     ],
-    plugins: ['react', '@typescript-eslint', 'prettier', 'import'],
-    env: {
-        browser: true,
-        es6: true,
+    'no-plusplus': 'off',
+    'react/state-in-constructor': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    '@typescript-eslint/no-unused-vars': 'error',
+    'react/static-property-placement': 'off',
+    'import/no-named-as-default': 'off',
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
+      {
+        'ts-expect-error': 'allow-with-description',
+        'ts-ignore': 'allow-with-description',
+        'ts-nocheck': 'allow-with-description',
+        'ts-check': 'allow-with-description',
+      },
+    ],
+    // 'import/no-extraneous-dependencies': [
+    //     'error',
+    //     {
+    //         devDependencies: [
+    //             'src/shared/utils/test.util.tsx',
+    //             'src/setupTests.ts',
+    //             'src/mocks/**/*',
+    //             '**/*.stories.tsx',
+    //             '**/*.test.tsx',
+    //         ],
+    //     },
+    // ],
+    'max-len': [0, 120, 2, { ignoreUrls: true }],
+    '@typescript-eslint/dot-notation': ['warn'],
+    '@typescript-eslint/no-explicit-any': ['error', { fixToUnknown: true, ignoreRestArgs: true }],
+    'no-console': ['error', { allow: ['error', 'info', 'warn'] }],
+    'no-param-reassign': ['error', { props: false }],
+    'react/destructuring-assignment': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/order': 'off',
+    // doesn't work with yup templates, example '${path} is required'
+    'no-template-curly-in-string': 'off',
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: [{ group: ['../../*'], message: 'usage of relative imports by more than one level not allowed' }],
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+      },
     },
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    ignorePatterns: ['.eslintrc.js', 'vite.config.js'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        project: ['./tsconfig.json'],
-    },
-    rules: {
-        'prettier/prettier': 'error',
-        'import/prefer-default-export': 'off',
-        'no-use-before-define': 'off',
-        'react/prop-types': 'off',
-        'react/jsx-filename-extension': 'off',
-        'react/no-unescaped-entities': [
-            'error',
-            {
-                forbid: ['>', '"', '}'],
-            },
-        ],
-        'no-plusplus': 'off',
-        'react/state-in-constructor': 'off',
-        '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
-        '@typescript-eslint/no-unused-vars': 'error',
-        'react/static-property-placement': 'off',
-        'import/no-named-as-default': 'off',
-        '@typescript-eslint/ban-ts-comment': [
-            'error',
-            {
-                'ts-expect-error': 'allow-with-description',
-                'ts-ignore': 'allow-with-description',
-                'ts-nocheck': 'allow-with-description',
-                'ts-check': 'allow-with-description',
-            },
-        ],
-        'import/no-extraneous-dependencies': [
-            'error',
-            {
-                devDependencies: [
-                    'src/shared/utils/test.util.tsx',
-                    'src/setupTests.ts',
-                    'src/mocks/**/*',
-                    '**/*.stories.tsx',
-                    '**/*.test.tsx',
-                ],
-            },
-        ],
-        'max-len': [0, 120, 2, { ignoreUrls: true }],
-        '@typescript-eslint/dot-notation': ['warn'],
-        '@typescript-eslint/no-explicit-any': ['error', { fixToUnknown: true, ignoreRestArgs: true }],
-        'no-console': ['error', { allow: ['error', 'info', 'warn'] }],
-        'no-param-reassign': ['error', { props: false }],
-        'react/destructuring-assignment': 'off',
-        'react/jsx-props-no-spreading': 'off',
-        'import/order': 'off',
-        // doesn't work with yup templates, example '${path} is required'
-        'no-template-curly-in-string': 'off',
-        'no-restricted-imports': [
-            'error',
-            {
-                patterns: [{ group: ['../../*'], message: 'usage of relative imports by more than one level not allowed' }],
-            },
-        ],
-    },
-    settings: {
-        'import/resolver': {
-            node: {
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-                moduleDirectory: ['node_modules', 'src/'],
-            },
-            typescript: {
-                alwaysTryTypes: true,
-            },
-        },
-    },
+  },
 };
---
+++ webui/vite.config.js
@@ -0,0 +1,70 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as fs from "fs";
+import * as ChildProcess from "child_process";
+import * as path from "path";
+import mime from "mime-types";
+import debug from "debug";
+
+const currentDir = "./picodata-ui";
+
+const walkSync = function (dir, filelist = []) {
+  const files = fs.readdirSync(dir);
+  const currentFileList = [...filelist];
+  files.forEach((file) => {
+    if (fs.statSync(dir + file).isDirectory()) {
+      currentFileList = walkSync(dir + file + "/", currentFileList);
+    } else {
+      currentFileList.push(dir + file);
+    }
+  });
+  return currentFileList;
+};
+
+const somePluginFunc = (currentNamespace = "") => {
+  const currentOptions = {
+    namespace: currentNamespace,
+    bundleName: "bundle.lua",
+    entryRegExp: /main.+js$/,
+    lua: "tarantool",
+  };
+  return {
+    name: "transform-bundle",
+    apply: "build",
+    writeBundle(options) {
+      const outputPath = options.dir;
+      const { bundleName, namespace, entryRegExp, lua } = currentOptions;
+      const buildFolder = path.relative(process.cwd(), outputPath);
+      const namespaceFolder =
+        buildFolder + "/assets/" + (namespace ? namespace + "/" : "");
+      const files = walkSync(namespaceFolder);
+      const filemap = {};
+      for (const file of files) {
+        const fileName = file.slice(namespaceFolder.length);
+        const fileBody = fs.readFileSync(file, { encoding: "utf8" });
+        filemap[fileName] = {
+          is_entry: entryRegExp.test(fileName),
+          body: fileBody,
+          mime: mime.lookup(fileName),
+        };
+      }
+      debug(path.join(currentDir, "pack-fron.lia"), "test");
+      const pathToPacker = "./pack-front.lua";
+      fs.writeFileSync(buildFolder + "/bundle.json", JSON.stringify(filemap), {
+        encoding: "utf8",
+      });
+      debug("compile bundle.json");
+      ChildProcess.execSync(
+        lua + " " + pathToPacker + " ./dist/bundle.json ./dist/" + bundleName
+      );
+      debug("dist " + bundleName);
+      fs.unlinkSync(buildFolder + "/bundle.json");
+      debug("delete bundle.json");
+    },
+  };
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), somePluginFunc()],
+});
