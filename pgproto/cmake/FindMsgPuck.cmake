667a295494ccf8c642d8f5b7221ba832c4f359cb
Dmitry Ivanov <ivadmi5@gmail.com>
2023-08-09 22:21:52 +0300

    fix(build): Various fixes to CMake build files

--- pgproto/cmake/FindMsgPuck.cmake
+++ pgproto/cmake/FindMsgPuck.cmake
@@ -3,16 +3,23 @@
 #
 #  MSGPUCK_FOUND - true if MsgPuck was found
 #  MSGPUCK_INCLUDE_DIRS - the directory of the MsgPuck headers
-#  MSGPUCK_LIBRARIES - the MsgPuck static library needed for linking
-#
+#  MSGPUCK_LIB - the MsgPuck static library needed for linking
 
-find_path(MSGPUCK_INCLUDE_DIR msgpuck.h PATH_SUFFIXES msgpuck)
-find_library(MSGPUCK_LIBRARY NAMES libmsgpuck.a)
+set(_MSGPUCK_ROOT_HINTS
+    HINTS ${MSGPUCK_ROOT_DIR})
+
+find_path(MSGPUCK_INCLUDE_DIRS
+    NAMES msgpuck.h
+    PATH_SUFFIXES msgpuck
+    ${_MSGPUCK_ROOT_HINTS})
+
+find_library(MSGPUCK_LIB
+    NAMES libmsgpuck.a
+    ${_MSGPUCK_ROOT_HINTS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MsgPuck
-    REQUIRED_VARS MSGPUCK_INCLUDE_DIR MSGPUCK_LIBRARY)
-set(MSGPUCK_INCLUDE_DIRS ${MSGPUCK_INCLUDE_DIR})
-set(MSGPUCK_LIBRARIES ${MSGPUCK_LIBRARY})
-mark_as_advanced(MSGPUCK_INCLUDE_DIR MSGPUCK_INCLUDE_DIRS
-                 MSGPUCK_LIBRARY MSGPUCK_LIBRARIES)
+    REQUIRED_VARS MSGPUCK_INCLUDE_DIRS MSGPUCK_LIB)
+
+set(MSGPUCK_FOUND TRUE)
+mark_as_advanced(MSGPUCK_INCLUDE_DIRS MSGPUCK_LIB)
