9a02e68904f65b01a6fd591f8cbc5f7a8ceca591
Dmitry Ivanov <ivadmi5@gmail.com>
2023-08-15 21:18:38 +0300

    Cleanup

--- pgproto/src/lib.rs
+++ pgproto/src/lib.rs
@@ -1,5 +1,22 @@
-use std::io::Read;
-use tarantool::log::TarantoolLogger;
+use bytes::{BufMut, BytesMut};
+use std::io::{self, Read};
+use tarantool::{
+    coio::{CoIOListener, CoIOStream},
+    log::TarantoolLogger,
+};
+
+fn read_into_buf(reader: &mut CoIOStream, buf: &mut impl BufMut) -> io::Result<usize> {
+    let slice = buf.chunk_mut();
+
+    // SAFETY: coio's Read impl won't read uninitialized bytes.
+    let cnt = unsafe {
+        let uninit = slice.as_uninit_slice_mut();
+        reader.read(std::mem::transmute(uninit))
+    }?;
+
+    unsafe { buf.advance_mut(cnt) }
+    Ok(cnt)
+}
 
 // This will be executed once the library is loaded.
 #[ctor::ctor]
@@ -12,55 +29,47 @@ fn setup_logger() {
 #[tarantool::proc]
 fn server_start() {
     log::info!("starting server...");
+    let server = server_bind();
 
+    // TODO: handle each client in a new fiber.
+    while let Ok(s) = server.accept() {
+        handle_client(s);
+    }
+}
+
+fn server_bind() -> CoIOListener {
     let mut socket = None;
     let mut f = |_| {
-        let raw = std::net::TcpListener::bind(("127.0.0.1", 5432)).unwrap();
-        let _ = socket.insert(raw);
+        let raw = match std::net::TcpListener::bind(("127.0.0.1", 5432)) {
+            Ok(listener) => listener,
+            Err(e) => {
+                log::error!("failed to bind postgres socket: {e}");
+                return -1;
+            }
+        };
+
+        log::info!("new postgres server: {raw:?}");
+        socket.replace(raw);
         0
     };
 
     let res = tarantool::coio::coio_call(&mut f, ());
     assert!(res == 0);
 
-    log::info!("{socket:?}");
-    let listener = tarantool::coio::CoIOListener::try_from(socket.unwrap()).unwrap();
-
-    while let Ok(mut s) = listener.accept() {
-        let mut buf = [0u8; 78];
-        s.read_exact(&mut buf).unwrap();
-        log::info!("YAY, BYTES: {buf:x?}");
-        let mut bytes = bytes::BytesMut::from(buf.as_ref());
-
-        let sockaddr = std::net::SocketAddr::from(([0; 4], 5432));
-        let mut codec = pgwire::tokio::PgWireMessageServerCodec::new(
-            pgwire::api::ClientInfoHolder::new(sockaddr, false),
-        );
-
-        use tokio_util::codec::Decoder;
-        let message = codec.decode(&mut bytes).unwrap().unwrap();
-        log::info!("YAY, MESSAGE: {message:?}");
-
-        handle_message(message);
-    }
+    let socket = socket.expect("uninitialized socket");
+    tarantool::coio::CoIOListener::try_from(socket).unwrap()
 }
 
-fn handle_message(message: pgwire::messages::PgWireFrontendMessage) {
-    use pgwire::messages::PgWireFrontendMessage::*;
-    match message {
-        Startup(_) => todo!(),
-        PasswordMessageFamily(_) => todo!(),
-        Query(_) => todo!(),
-        Parse(_) => todo!(),
-        Close(_) => todo!(),
-        Bind(_) => todo!(),
-        Describe(_) => todo!(),
-        Execute(_) => todo!(),
-        Flush(_) => todo!(),
-        Sync(_) => todo!(),
-        Terminate(_) => todo!(),
-        CopyData(_) => todo!(),
-        CopyFail(_) => todo!(),
-        CopyDone(_) => todo!(),
-    }
+fn handle_client(mut client: CoIOStream) {
+    let mut buf = BytesMut::with_capacity(8192);
+    let cnt = read_into_buf(&mut client, &mut buf).unwrap();
+    log::info!("read {cnt} bytes");
+
+    log::info!("raw message: {buf:x?}");
+    use pgwire::messages::Message;
+    let message = pgwire::messages::startup::Startup::decode(&mut buf)
+        .unwrap()
+        .unwrap();
+
+    log::info!("parsed message: {message:?}");
 }
