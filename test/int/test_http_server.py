1c6dd9479008ad01c075e1993b7d8c6d7bb47563
Anatoly Popov <a.popov@picodata.io>
2023-12-08 19:29:49 +0300

    feat(webui): add tiers to picodata http-server
    1. http-server:
    
        1. Get info about tiers from _pico_tier
        2. Get info about instance addresses from:
            - http: lua api
            - binary:  _pico_peer_addresses
        3. Switch from arrays to map where it makes sense
        4. Compose all the data to `cluster_state` object
        5. Get version information for lua api.
    
    2. Tests:
    
        1. Add DeepDiff package to better diff for json
        2. Amend tests to new specification
        3. Get picodata version via lua api and use it in asserts
    
    3. Chore:
    
        1. Add .envrc and .direnv to .gitingore (direnv specific files)
        2. Add .vscode to .gitignore.
    
    Signed-off-by: Anatoly Popov <a.popov@picodata.io>

--- test/int/test_http_server.py
+++ test/int/test_http_server.py
@@ -1,3 +1,4 @@
+from deepdiff import DeepDiff  # type: ignore
 from conftest import Cluster, Instance
 from urllib.request import urlopen
 import pytest
@@ -32,45 +33,67 @@ def test_http_routes(instance: Instance):
 @pytest.mark.webui
 def test_webui(instance: Instance):
     http_listen = instance.env["PICODATA_HTTP_LISTEN"]
+
+    instance_version = instance.eval("return pico.PICODATA_VERSION")
+
     with urlopen(f"http://{http_listen}/") as response:
         assert response.headers.get("content-type") == "text/html"
 
-    with urlopen(f"http://{http_listen}/api/v1/replicaset") as response:
+    with urlopen(f"http://{http_listen}/api/v1/tiers") as response:
         assert response.headers.get("content-type") == "application/json"
-        assert json.load(response) == [
-            {
-                "grade": "Online",
-                "version": "??.??",
-                "instances": [
-                    {
-                        "failureDomain": {},
-                        "isLeader": True,
-                        "currentGrade": "Online",
-                        "targetGrade": "Online",
-                        "name": "i1",
-                        "version": "??.??",
-                    }
-                ],
-                "instanceCount": 1,
-                "capacityUsage": 100,
-                "memory": {
-                    "usable": 33554432,
-                    "used": 33554432,
-                },
-                "uuid": instance.replicaset_uuid(),
-                "id": "r1",
-            }
-        ]
+        diff = DeepDiff(
+            json.load(response),
+            [
+                {
+                    "replicasets": [
+                        {
+                            "grade": "Online",
+                            "version": instance_version,
+                            "instances": [
+                                {
+                                    "failureDomain": {},
+                                    "isLeader": True,
+                                    "currentGrade": "Online",
+                                    "targetGrade": "Online",
+                                    "name": "i1",
+                                    "version": instance_version,
+                                    "httpAddress": http_listen,
+                                    "binaryAddress": instance.listen,
+                                }
+                            ],
+                            "instanceCount": 1,
+                            "capacityUsage": 100,
+                            "memory": {
+                                "usable": 33554432,
+                                "used": 33554432,
+                            },
+                            "uuid": instance.replicaset_uuid(),
+                            "id": "r1",
+                        }
+                    ],
+                    "replicasetCount": 1,
+                    "rf": 1,
+                    "instanceCount": 1,
+                    "can_vote": True,
+                    "name": "default",
+                    "plugins": [],
+                }
+            ],
+        )
+        assert len(diff) == 0, diff
 
     with urlopen(f"http://{http_listen}/api/v1/cluster") as response:
         assert response.headers.get("content-type") == "application/json"
         response = json.load(response)
-        version = response.pop("currentInstaceVersion")
-        assert response == {
-            "capacityUsage": 100,
-            "replicasetsCount": 1,
-            "instancesCurrentGradeOffline": 0,
-            "memory": {"usable": 33554432, "used": 33554432},
-            "instancesCurrentGradeOnline": 1,
-        }
-        assert version.startswith("23.12.0")
+        diff = DeepDiff(
+            response,
+            {
+                "capacityUsage": 100,
+                "replicasetsCount": 1,
+                "instancesCurrentGradeOffline": 0,
+                "currentInstaceVersion": instance_version,
+                "memory": {"usable": 33554432, "used": 33554432},
+                "instancesCurrentGradeOnline": 1,
+            },
+        )
+        assert len(diff) == 0, diff
