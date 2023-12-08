8150522c0ee629210d7f15db85ddabab7b739072
Anatoly Popov <a.popov@picodata.io>
2023-12-08 19:29:49 +0300

    feat(webui): add tiers to picodata http-server
    1. http-server:
    
        1. Get info about tiers from _pico_tier
        2. Get info about instance addresses from:
            - http: lua api
            - binary:  _pico_peer_addresses
        3. Switch from arrays to map where it makes sense
        4. Compose all the data to `cluster_state` object
        5. Get version information for lua api.
    
    2. Tests:
    
        1. Add DeepDiff package to better diff for json
        2. Amend tests to new specification
        3. Get picodata version via lua api and use it in asserts
    
    3. Chore:
    
        1. Add .envrc and .direnv to .gitingore (direnv specific files)
        2. Add .vscode to .gitignore.
    
    Signed-off-by: Anatoly Popov <a.popov@picodata.io>
    (cherry picked from commit 1c6dd9479008ad01c075e1993b7d8c6d7bb47563)

--- src/http_server.lua
+++ src/http_server.lua
@@ -1,5 +1,6 @@
 local fun = require('fun')
 local json = require('json')
+local net = require('net.box')
 
 -- I believe that we won't maintain clusters over 1k replicasets
 -- in the nearest future
@@ -32,8 +33,49 @@ end
 local function get_replicasets()
     local tuples = box.space._pico_replicaset:select({}, { limit = SELECT_LIMIT })
     return fun.iter(tuples)
-        :map(function(tuple) return tuple:tomap({ names_only = true }) end)
-        :totable()
+        :map(function(tuple)
+            local replicaset = tuple:tomap({ names_only = true })
+            return replicaset.replicaset_uuid, replicaset
+        end)
+        :tomap()
+end
+
+local function get_peer_addresses()
+    local tuples = box.space._pico_peer_address:select({}, { limit = SELECT_LIMIT })
+    return fun.iter(tuples)
+        :map(function(tuple)
+            local peer_address = tuple:tomap({ names_only = true })
+            return peer_address.raft_id, peer_address
+        end)
+        :tomap()
+end
+
+--[[ the expected response:
+export type TierType = {
+    id: string;
+    plugins: string[];
+    replicasetCount: number;
+    instanceCount: number;
+    rf: number;
+    canVoter: boolean;
+    replicasets: ReplicasetType[];
+};
+]]
+local function get_tiers()
+    local tuples = box.space._pico_tier:select({}, { limit = SELECT_LIMIT })
+    return fun.iter(tuples)
+        :map(function(tuple)
+            local tier = tuple:tomap({ names_only = true })
+            tier.can_vote = tier.can_vote or true
+            tier.replicasets = {}
+            tier.plugins = {}
+            tier.replicasetCount = 0
+            tier.instanceCount = 0
+            tier.rf = tier.replication_factor
+            tier.replication_factor = nil
+            return tier.name, tier
+        end)
+        :tomap()
 end
 
 -- returns leader's box.slab.info
@@ -64,90 +106,145 @@ local function get_memory_info()
 end
 
 local function is_leader(instance, replicasets)
-    local replicaset = fun.iter(replicasets)
-        :filter(function(replicaset) return replicaset.replicaset_uuid == instance.replicaset_uuid end)
-        :totable()[1]
-
-    return instance.instance_id == replicaset.master_id
+    return instance.instance_id == replicasets[instance.replicaset_uuid].master_id
 end
 
-local function cast_instance(instance, is_leader)
-    return {
+local function get_address(instance, peer_addresses)
+    return peer_addresses[instance.raft_id].address
+end
+
+--[[
+    export interface InstanceType {
+    name: string;
+    targetGrade: string;
+    currentGrade: string;
+    failureDomain: Record<string, string>;
+    version: string;
+    isLeader: boolean;
+    binaryAddress: string;
+    httpAddress: string;
+}
+]]
+local function create_instance(instance, cluster_state)
+    local instance_dto = {
         name = instance.instance_id,
         targetGrade = instance.target_grade[1],
         currentGrade = instance.current_grade[1],
         failureDomain = instance.failure_domain,
         version = VERSION_PLACEHOLDER,
-        isLeader = is_leader,
+        isLeader = is_leader(instance, cluster_state.replicasets),
+        binaryAddress = get_address(instance, cluster_state.peer_addresses),
+    }
+
+    local c = net.connect(instance_dto.binaryAddress, { fetch_schema = false })
+    local result = c:eval([[
+        return {
+            httpd = pico and pico.httpd and {
+                host = pico.httpd.host,
+                port = pico.httpd.port
+            },
+            version = pico.PICODATA_VERSION
+        }
+    ]])
+
+    if result then
+        local httpAddress = result.httpd
+        if httpAddress then
+            instance_dto.httpAddress = string.format('%s:%d', httpAddress.host, httpAddress.port)
+        end
+        instance_dto.version = result.version
+    end
+
+    return instance_dto
+end
+
+--[[
+export interface ReplicasetType {
+    id: string;
+    instanceCount: number;
+    instances: InstanceType[];
+    version: string;
+    grade: string;
+    capacity: number;
+}
+]]
+local function update_replicaset_memory_data(replicaset, instance, instance_memory_info)
+    replicaset.grade = instance.current_grade[1]
+    -- frontend expects number
+    replicaset.capacityUsage = instance_memory_info.quota_used / instance_memory_info.quota_size * 100
+    replicaset.memory = {
+        used = instance_memory_info.quota_used,
+        usable = instance_memory_info.quota_size,
     }
 end
 
--- the expected response:
--- --------------------------------------------------
--- export interface ReplicasetType {
---     id: string;
---     instanceCount: number;
---     instances: InstanceType[];
---     version: string;
---     grade: string;
---     capacity: string;
--- }
+local function create_replicaset(instance, cluster_state, instance_memory_info)
+    local instance_dto = create_instance(instance, cluster_state)
+    local replicaset = {
+        id = instance.replicaset_id,
+        uuid = instance.replicaset_uuid,
+        instanceCount = 1,
+        instances = { instance_dto },
+        version = instance_dto.version,
+        tier = instance.tier
+    }
 
--- export interface InstanceType {
---     name: string;
---     targetGrade: string;
---     currentGrade: string;
---     failureDomain: string;
---     version: string;
---     isLeader: boolean;
---   }
--- --------------------------------------------------
-local function list_replicasets()
-    local replicasets = get_replicasets()
+    if is_leader(instance, cluster_state.replicasets) then
+        update_replicaset_memory_data(replicaset, instance, instance_memory_info)
+    end
 
-    local instances = get_instances()
+    return replicaset
+end
 
-    local memory_info = get_memory_info()
-    local replicaset_table = fun.iter(instances)
-        :reduce(function(acc, instance)
-            local is_leader = is_leader(instance, replicasets)
-            local instance_memory_info = memory_info[instance.replicaset_uuid]
-            if acc[instance.replicaset_uuid] == nil then
-                acc[instance.replicaset_uuid] = {
-                    id = instance.replicaset_id,
-                    uuid = instance.replicaset_uuid,
-                    instanceCount = 1,
-                    instances = { cast_instance(instance, is_leader) },
-                    version = VERSION_PLACEHOLDER,
-                    grade = is_leader and instance.current_grade[1],
-                    -- frontend expects number
-                    capacityUsage =
-                        is_leader
-                        and instance_memory_info.quota_used / instance_memory_info.quota_size * 100,
-                    memory = is_leader and {
-                        used = instance_memory_info.quota_used,
-                        usable = instance_memory_info.quota_size,
-                    },
-                }
-            else
-                local replicaset = acc[instance.replicaset_uuid]
-                table.insert(replicaset.instances, cast_instance(instance))
-                replicaset.instanceCount = replicaset.instanceCount + 1
-                if is_leader then
-                    replicaset.grade = instance.current_grade[1]
-                    replicaset.capacity = instance_memory_info.quota_used / instance_memory_info.quota_size * 100
+local function list_tiers()
+    local cluster_state = {
+        replicasets = get_replicasets(),
+        instances = get_instances(),
+        memory_info = get_memory_info(),
+        tiers = get_tiers(),
+        peer_addresses = get_peer_addresses()
+    }
+
+
+    local replicasets = (fun.iter(cluster_state.instances)
+        :reduce(
+            function(acc, instance)
+                local instance_memory_info = cluster_state.memory_info[instance.replicaset_uuid]
+                if acc[instance.replicaset_uuid] == nil then
+                    acc[instance.replicaset_uuid] = create_replicaset(
+                        instance,
+                        cluster_state,
+                        instance_memory_info)
+                else
+                    local replicaset = acc[instance.replicaset_uuid]
+                    table.insert(replicaset.instances, create_instance(instance, cluster_state))
+                    replicaset.instanceCount = replicaset.instanceCount + 1
+                    if is_leader(instance, cluster_state.replicasets) then
+                        update_replicaset_memory_data(replicaset, instance, instance_memory_info)
+                    end
                 end
-            end
-            return acc
-        end, {})
+                return acc
+            end,
+            {}))
 
-    local replicasets = fun.iter(replicaset_table)
-        :map(function(_, replicaset) return replicaset end)
-        :totable()
+    local tiers = fun.iter(replicasets)
+        :reduce(
+            function(acc, _, replicaset)
+                local tier = acc[replicaset.tier]
+                tier.instanceCount = tier.instanceCount + replicaset.instanceCount
+                tier.replicasetCount = tier.replicasetCount + 1
+                table.insert(tier.replicasets, replicaset)
+                replicaset.tier = nil
+                return acc
+            end,
+            cluster_state.tiers
+        )
 
     return {
         status = 200,
-        body = json.encode(replicasets),
+        body = json.encode(fun.iter(tiers)
+            :map(function (_, tier) return tier end)
+            :totable()),
         headers = {
             ['content-type'] = 'application/json' }
         }
@@ -168,7 +265,7 @@ end
 -- }
 -- --------------------------------------------------
 local function cluster_info()
-    local replicasets = get_replicasets()
+    local replicasets = fun.iter(get_replicasets()):map(function(_, x) return x end):totable()
     local replicasetsCount = table_len(replicasets)
 
     local instances = get_instances()
@@ -216,7 +313,7 @@ end
 
 local host, port = ...;
 local httpd = require('http.server').new(host, port);
-httpd:route({ method = 'GET', path = 'api/v1/replicaset' }, list_replicasets)
+httpd:route({ method = 'GET', path = 'api/v1/tiers' }, list_tiers)
 httpd:route({ method = 'GET', path = 'api/v1/cluster' }, cluster_info)
 httpd:start();
 _G.pico.httpd = httpd
