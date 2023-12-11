5e298fd5487ae91bb3d0e491159e8c2b968274bb
a.sabirov <a.sabirov@picodata.io>
2023-12-11 20:03:04 +0300

    rename: filter_plugin_records -> hooks/filter_logs

--- hooks/filter_logs.py
+++ hooks/filter_logs.py
@@ -1,10 +1,12 @@
 from logging import LogRecord
 from mkdocs import utils
 
+
 def filter_inline_svg(record: LogRecord) -> bool:
     if record.msg.startswith("mkdocs-plugin-inline-svg"):
         return False
     return True
+
 
 def on_page_markdown(markdown, **kwargs):
     utils.log.addFilter(filter_inline_svg)
