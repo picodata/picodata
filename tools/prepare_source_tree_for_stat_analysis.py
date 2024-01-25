5deeca06f5c0dbc25cc3c9fa11d4af4de22a23ed
Dmitry Rodionov <d.rodionov@picodata.io>
2024-01-25 22:05:33 +0300

    Pick up stat analysis patches made for 23.12 and 2.11.0
    This patch combines patches from these two picodata/tarantool fork branches:
    https://git.picodata.io/picodata/tarantool/-/tree/dynamic-linking-from-system?ref_type=heads
    https://git.picodata.io/picodata/picodata/picodata/-/tree/svace-fixes?ref_type=heads
    
    Previously patches for tarantool were made for 2.11.0 so for 2.11.2 (version we currently use)
    some of them had to be updated.
    
    Patches from b124e8f01fafb98bc3005f77b8852d1f699f8666 initially were committed
    to fork repo but for manageability it was decided that its easier to support
    those via patch files. So these changes are added to existing patches in
    certification_patches dir.
    
    Then follow changes from 6b851b86a20818981e28962a7ac2a0764b21b2d4
    and 2be2aab5096b202de8bab72bafe41470c479895d. Patch application is
    currently concentrated in gitlab-ci.yml. 6b851b moved svace patches
    application to tarantool's makefile. For now its easier to keep it
    in one place. Svace patches from previous commit are moved into
    subdirectory. Patches that touch supplementary files or simply remove
    them are removed and the list of the files is kept for later to form a
    dedicated list of files for deletion.
    
    Conflicting were picked from f98334ea66de2f61866cd33d97f1717738fc841b.
    Redundant patch for curl libssh was removed because the same change
    appeared upstream https://github.com/curl/curl/pull/11088/files
    
    Luagdb patch was updated.
    OSCP response patch as well as icu_common_exec patch were removed,
    since original files are not needed in the build process and thus are
    deleted from the repo.
    
    Lastly patch application is moved to dedicated python script for
    ease of sharing with vm builder repo.
    
    Co-authored-by: Feodor Alexandrov <feodor.alex.alexandrov@gmail.com>

---
+++ tools/prepare_source_tree_for_stat_analysis.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import argparse
+from contextlib import contextmanager
+import glob
+import os
+from pathlib import Path
+import subprocess  # nosec
+import sys
+
+REPO_DIR = Path(__file__).parent.parent
+PATCHES_DIR = REPO_DIR / "certification_patches"
+SVACE_PATCHES = PATCHES_DIR / "svace_patches"
+GAMAYUN_PATCHES = PATCHES_DIR / "gamayun_patches"
+TARANTOOL_SYS = REPO_DIR / "tarantool-sys"
+THIRD_PARTY = TARANTOOL_SYS / "third_party"
+
+DEBUG = os.getenv("DEBUG")
+
+# List of files that need to be deleted before stat analysis
+# Usually these are supplementary files from third party libraries
+# like test data, utility scripts etc.
+# This is needed because we cant use exclusion rules for static
+# analysis tools so we need to remove unwanted files from the artifact
+DEAD_LIST = [
+    # from 6b2a88b551e6940089cf248d88b050b65ab67262
+    "tarantool-sys/vendor/icu4c-71_1/source/python/icutools/databuilder/renderers/common_exec.py",
+    "tarantool-sys/third_party/nghttp2/script/fetch-ocsp-response",
+    "tarantool-sys/vendor/openssl-1.1.1q/fuzz/helper.py",
+    "tarantool-sys/third_party/zstd/.circleci/images/primary/Dockerfile",
+    # from 2be2aab5096b202de8bab72bafe41470c479895d
+    "tarantool-sys/third_party/nghttp2/src/ca.nghttp2.org-key.pem",
+    "tarantool-sys/third_party/nghttp2/src/ca.nghttp2.org.pem",
+    "tarantool-sys/third_party/nghttp2/src/test.example.com-key.pem",
+    "tarantool-sys/third_party/nghttp2/src/test.example.com.pem",
+    "tarantool-sys/third_party/nghttp2/src/test.nghttp2.org-key.pem",
+    "tarantool-sys/third_party/nghttp2/src/test.nghttp2.org.pem",
+    "tarantool-sys/third_party/curl/packages/OS400/initscript.sh",
+    "tarantool-sys/third_party/decNumber/example*.c",
+    "tarantool-sys/third_party/libeio/install-sh",
+    "tarantool-sys/third_party/libeio/ltmain.sh",
+    "tarantool-sys/third_party/libev/depcomp",
+    "tarantool-sys/third_party/libev/install-sh",
+    "tarantool-sys/third_party/libev/ltmain.sh",
+    "tarantool-sys/tools/gen-release-notes",
+    "sbroad/docker-compose.yml",
+]
+
+
+@contextmanager
+def cd(target: Path):
+    old = os.getcwd()
+    os.chdir(target)
+    yield
+    os.chdir(old)
+
+
+def apply(patch: Path):
+    subprocess.check_call(["git", "apply", str(patch)])  # nosec
+
+
+def apply_from_dir(path: Path):
+    for patch in path.iterdir():
+        patch = patch.resolve()
+        print("Applying:", patch)
+
+        libname = patch.stem.split("_", maxsplit=1)[0]
+
+        if libname == "tarantool-sys":
+            with cd(TARANTOOL_SYS):
+                apply(patch)
+        elif libname in ("http", "vshard"):
+            with cd(libname):
+                apply(patch)
+        else:
+            with cd(THIRD_PARTY / libname):
+                apply(patch)
+
+
+def prepare_for_gamayun():
+    for glob_pattern in DEAD_LIST:
+        for fname in glob.glob(glob_pattern):
+            print("Removing:", REPO_DIR / fname)
+
+            (REPO_DIR / fname).unlink(missing_ok=DEBUG)
+
+    apply_from_dir(GAMAYUN_PATCHES)
+
+
+def prepare_for_svace():
+    apply_from_dir(SVACE_PATCHES)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "Apply various certification induced transformations to source tree"
+    )
+    subparsers = parser.add_subparsers(dest="command")
+    subparsers.add_parser("gamayun", help="Prepare for gamayun analysis")
+    subparsers.add_parser("svace", help="Prepare for svace analysis")
+
+    commands = {"gamayun": prepare_for_gamayun, "svace": prepare_for_svace}
+
+    args = parser.parse_args()
+    command = commands.get(args.command)
+    if command is None:
+        print(f"Unknown command: {command}", file=sys.stderr)
+        sys.exit(1)
+
+    command()
