c98473219467181b2ca0233eb81dc59ea33648c7
Georgy Moshkin <gmoshkin@picodata.io>
2024-03-05 20:42:44 +0300

    feat: pico_proc_macro subcrate for #[derive(Introspection)]

---
+++ pico_proc_macro/src/lib.rs
@@ -0,0 +1,30 @@
+use quote::quote;
+
+#[proc_macro_derive(Introspection)]
+pub fn derive_introspection(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    let name = &input.ident;
+
+    let mut field_names = vec![];
+    match &input.data {
+        syn::Data::Struct(ds) => match &ds.fields {
+            syn::Fields::Named(fs) => {
+                for field in &fs.named {
+                    let field_name = field.ident.as_ref().unwrap();
+                    field_names.push(field_name.to_string());
+                }
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+
+    quote! {
+        impl #name {
+            pub const FIELD_NAMES: &'static [&'static str] = &[
+                #( #field_names, )*
+            ];
+        }
+    }
+    .into()
+}
