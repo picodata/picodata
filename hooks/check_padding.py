332c5cd8f4b0ec8eaccc2dcc1fbcead12e90ff12
a.sabirov <a.sabirov@picodata.io>
2024-03-06 18:59:31 +0300

    chore: reformat check_padding.py

--- hooks/check_padding.py
+++ hooks/check_padding.py
@@ -16,17 +16,17 @@ def on_page_markdown(markdown: str, page: Page, config: MkDocsConfig, files: Fil
 
     for n, line in enumerate(lines):
         if re.match("^##+ ", line):
-            prev = lines[n-1]
-            next = lines[n+1]
+            prev = lines[n - 1]
+            next = lines[n + 1]
 
             if re.match("^---+$", prev):
-                prev = lines[n-2]
+                prev = lines[n - 2]
 
             if prev != "" or next != "":
                 log.warning(f"MISSING PADDING AROUND HEADER @ {path}")
                 log.info("")
-                log.info(lines[n-2])
-                log.info(lines[n-1])
+                log.info(lines[n - 2])
+                log.info(lines[n - 1])
                 log.info(lines[n])
-                log.info(lines[n+1])
+                log.info(lines[n + 1])
                 log.info("")
