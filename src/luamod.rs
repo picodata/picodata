c01febdf461d9315b416221215dcddf5adddb535
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2024-05-24 15:50:27 +0300

    git: bump version to 24.3.1

--- src/luamod.rs
+++ src/luamod.rs
@@ -62,11 +62,11 @@ pub(crate) fn setup(config: &PicodataConfig) {
 
             picodata> pico.PICODATA_VERSION
             ---
-            - 24.2.2
+            - 24.3.1
             ...
         "},
         {
-            const _: () = assert!(str_eq(env!("CARGO_PKG_VERSION"), "24.2.2"));
+            const _: () = assert!(str_eq(env!("CARGO_PKG_VERSION"), "24.3.1"));
             crate::info::PICODATA_VERSION
         },
     );
