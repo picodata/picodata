1d1ed8f40e251b098bcee6b377ddaf68fda70d8d
a.sabirov <a.sabirov@picodata.io>
2024-07-19 19:41:19 +0300

    exclude_sitemap.py: delete hook

--- hooks/exclude_sitemap.py
+++
@@ -1,6 +0,0 @@
-from mkdocs.config.defaults import MkDocsConfig
-from mkdocs.structure.files import Files
-
-
-def on_files(files: Files, config: MkDocsConfig):
-    config["theme"].static_templates.remove("sitemap.xml")
