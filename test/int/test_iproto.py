239e654a395ac8f4e10d558d06359858f1b7a1b4
Кирилл Безуглый <k.bezuglyi@picodata.io>
2024-09-20 20:09:45 +0300

    test: add tests for forbidden IPROTO types

--- test/int/test_iproto.py
+++ test/int/test_iproto.py
@@ -3,35 +3,42 @@ import pytest
 from conftest import Instance, Connection, MalformedAPI, TarantoolError, ErrorCode
 
 
+USER_NAME = "kelthuzad"
+USER_PASS = "g$$dP4ss"
 TABLE_NAME = "warehouse"
 
 
-def create_connection(instance: Instance):
-    user_name = "kelthuzad"
-    user_pass = "g$$dP4ss"
-
+def create_connection(instance: Instance, use_call_16: bool = False):
     dcl = instance.sql(
         f"""
-        CREATE USER {user_name} WITH PASSWORD '{user_pass}' USING chap-sha1
+        CREATE USER {USER_NAME} WITH PASSWORD '{USER_PASS}' USING chap-sha1
     """
     )
     assert dcl["row_count"] == 1
-    acl = instance.sql(f"GRANT READ ON TABLE {TABLE_NAME} TO {user_name}")
-    assert acl["row_count"] == 1
-    acl = instance.sql(f"GRANT WRITE ON TABLE {TABLE_NAME} TO {user_name}")
-    assert acl["row_count"] == 1
 
     conn = Connection(
         instance.host,
         instance.port,
-        user=user_name,
-        password=user_pass,
+        user=USER_NAME,
+        password=USER_PASS,
         connect_now=True,
         reconnect_max_attempts=0,
+        call_16=use_call_16,
     )
     assert conn
 
     return conn
+
+
+def test_iproto_forbidden(instance: Instance):
+    conn = create_connection(instance, use_call_16=True)
+
+    with pytest.raises(TarantoolError) as data:
+        conn.call("fake_function")
+    assert data.value.args[:2] == (
+        "ER_UNSUPPORTED",
+        "picodata does not support IPROTO_CALL_16 request type",
+    )
 
 
 def test_iproto_execute(instance: Instance):
@@ -65,14 +72,10 @@ def test_iproto_execute(instance: Instance):
         conn.execute(f"SELECT * FROM {TABLE_NAME}")
     assert data.value.args[:2] == (
         "ER_ACCESS_DENIED",
-        f"Read access to space '{TABLE_NAME}' is denied for user 'guest'",
+        f"Read access to space '{TABLE_NAME}' is denied for user '{USER_NAME}'",
     )
 
-    acl = instance.sql(
-        f"""
-        GRANT READ ON TABLE {TABLE_NAME} TO guest
-    """
-    )
+    acl = instance.sql(f"GRANT READ ON TABLE {TABLE_NAME} TO {USER_NAME}")
     assert acl["row_count"] == 1
 
     with pytest.raises(MalformedAPI) as dql:
@@ -83,14 +86,10 @@ def test_iproto_execute(instance: Instance):
         conn.execute(f"SELECT * FRUM {TABLE_NAME}")
     assert dql.value.args[:2] == (
         ErrorCode.Other,
-        f"sbroad: rule parsing error:  --> 1:8\n  |\n1 | SELECT * FRUM {TABLE_NAME}\n  |        ^---\n  |\n  = expected Identifier or Distinct",  # noqa: E501
+        f"sbroad: rule parsing error:  --> 1:10\n  |\n1 | SELECT * FRUM {TABLE_NAME}\n  |          ^---\n  |\n  = expected EOI, Limit, UnionOp, ExceptOp, or UnionAllOp",  # noqa: E501
     )
 
-    acl = instance.sql(
-        f"""
-        GRANT WRITE ON TABLE {TABLE_NAME} TO guest
-    """
-    )
+    acl = instance.sql(f"GRANT WRITE ON TABLE {TABLE_NAME} TO {USER_NAME}")
     assert acl["row_count"] == 1
 
     dml = conn.execute(f"DELETE FROM {TABLE_NAME} WHERE id = 1")
