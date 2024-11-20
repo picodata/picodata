21b861beacd6163cdc258f0d3403e396f265cd6d
Dmitry Ivanov <ivadmi5@gmail.com>
2024-11-21 02:03:40 +0300

    fix(test/inner): panic if failed to exec picodata

--- test/inner.rs
+++ test/inner.rs
@@ -1,8 +1,10 @@
 use std::os::unix::process::CommandExt;
 
 fn main() {
-    std::process::Command::new(env!("CARGO_BIN_EXE_picodata"))
+    let err = std::process::Command::new(env!("CARGO_BIN_EXE_picodata"))
         .arg("test")
         .args(std::env::args().skip(1))
         .exec();
+
+    panic!("failed to exec: {err}");
 }
