7e9e4ff0d1393ed33f15da26dd10a71f1459719b
Dmitry Rodionov <d.rodionov@picodata.io>
2024-12-26 21:22:56 +0300

    test: add test logs to stderr too

--- test/framework/log.py
+++ test/framework/log.py
@@ -1,5 +1,6 @@
 import logging
 import logging.config
+import sys
 
 LOGGING = {
     "version": 1,
@@ -11,3 +12,12 @@ LOGGING = {
 logging.config.dictConfig(LOGGING)
 
 log = logging.getLogger("root")
+
+handler = logging.StreamHandler(sys.stderr)
+handler.setLevel(logging.INFO)
+# same as in pytest.ini
+formatter = logging.Formatter(
+    "%(asctime)s.%(msecs)-3d %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
+)
+handler.setFormatter(formatter)
+log.addHandler(handler)
