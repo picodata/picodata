ae580e097b810cdc34255b5bd895d5a42f9b4de9
Georgy Moshkin <gmoshkin@picodata.io>
2025-02-26 14:39:43 +0300

    test: fix log format when there's fewer than 100 milliseconds

--- test/framework/log.py
+++ test/framework/log.py
@@ -16,6 +16,9 @@ log = logging.getLogger("root")
 handler = logging.StreamHandler(sys.stderr)
 handler.setLevel(logging.INFO)
 # same as in pytest.ini
-formatter = logging.Formatter("%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s")
+formatter = logging.Formatter(
+    "%(asctime)s.%(msecs)03d %(levelname)s [%(filename)s:%(lineno)d] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 handler.setFormatter(formatter)
 log.addHandler(handler)
