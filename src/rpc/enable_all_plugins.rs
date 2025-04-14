8de053b47720b45249eae53e1212b06195c8ff81
Кирилл Безуглый <k.bezuglyi@picodata.io>
2025-04-14 16:47:50 +0300

    fix(rpc): governor calls old rpc to enable plugins
    Right after the release of 25.2.0 (8dbc7434), we have broken the
    backwards compatibility by renaming `proc_enable_all_plugins` to
    `proc_before_online`. Some time passed (1ba919e1), and we've returned
    it back, while also redirecting it to a new implementation. Now, we
    have to make governor use the old RPC, so that we do not break the
    compatibility anymore.
    
    Closes #1433.
    Follow-up for !1662.

--- src/rpc/enable_all_plugins.rs
+++ src/rpc/enable_all_plugins.rs
@@ -2,12 +2,12 @@ use crate::rpc::before_online::{self, before_online_inner};
 
 crate::define_rpc_request! {
     /// !SOFT DEPRECATED!
-    /// For actual RPC, see `proc_before_online`.
+    /// For the actual RPC, see [`crate::rpc::before_online::proc_before_online`].
     fn proc_enable_all_plugins(req: Request) -> crate::traft::Result<Response> {
         before_online_inner(req.0).map(Response)
     }
 
-    pub struct Request(before_online::Request);
+    pub struct Request(pub before_online::Request);
 
-    pub struct Response(before_online::Response);
+    pub struct Response(pub before_online::Response);
 }
