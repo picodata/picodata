8eff300181b7e872144ac82dffc1f34900e9f908
Yegor Podlesov <ypodlesov@gmail.com>
2025-05-13 12:04:47 +0000

    fix(sql): make correct trim sql function processing

--- test/int/sql/trim.sql
+++ test/int/sql/trim.sql
@@ -45,3 +45,33 @@ SELECT trim(trailing 'b' from trim('aabb')) from "t"
 SELECT trim(both 'ab' from 'aabb') from "t"
 -- EXPECTED:
 ''
+
+-- TEST: test_trim-1.8
+-- SQL:
+SELECT trim(  'a' from trim(  '  aabb  ')) from "t"
+-- EXPECTED:
+'bb'
+
+-- TEST: test_trim-1.9
+-- SQL:
+SELECT trim(  trim(  ' aabb ') from trim(  '  aabb  ')) from "t"
+-- EXPECTED:
+''
+
+-- TEST: test_trim-1.10
+-- SQL:
+SELECT trim(  leading 'a' from trim(  'aabb  ')) from "t"
+-- EXPECTED:
+'bb'
+
+-- TEST: test_trim-1.11
+-- SQL:
+SELECT trim(  trailing 'b' from trim(  'aabb')) from "t"
+-- EXPECTED:
+'aa'
+
+-- TEST: test_trim-1.12
+-- SQL:
+SELECT trim(  both 'ab' from 'aabb') from "t"
+-- EXPECTED:
+''
