c945e8c10f718500ff8acc8421ca0b82cf61078d
Vitaly Shunkov <v.shunkov@picodata.io>
2025-06-11 17:28:07 +0300

    ci: add stage clone
    Faster clone using master's working directory cache.
    Master push cache of working directory on clone stage (this will happen once
    per day or more). In dev branches, the cache is taken from the master by
    fallback_keys and the current commit is fetched. In dev branches cloning via
    gitlab is disabled in tests and lint, and implemented via git clone/fetch.

---
+++ tools/clone.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# get start time
+start_time=$(date +%s)
+
+# check if .git exist, fetch current commit and update submodules
+if [ -d "$CI_PROJECT_DIR/.git" ]; then
+  cd $CI_PROJECT_DIR
+  # parallel clone/fetch
+  git config fetch.parallel 8
+  git clean -fdx
+  git branch -D $CI_COMMIT_REF_NAME || echo "$CI_COMMIT_REF_NAME doesn't exist"
+  git fetch -uf origin $CI_COMMIT_REF_NAME:$CI_COMMIT_REF_NAME
+  git checkout $CI_COMMIT_REF_NAME
+  git submodule update --init --recursive
+else
+  # do full clone
+  git clone --recursive -b $CI_COMMIT_REF_NAME -j8 https://git.picodata.io/${CI_PROJECT_PATH}.git $CI_PROJECT_DIR
+fi
+
+# get end time and difference
+date
+end_time=$(date +%s)
+difference=$((end_time - start_time))
+echo "Clone took: $difference seconds"
