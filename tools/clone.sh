cfbe992bf09b6a21f37b52b0a5f2f3e87514fa52
Dmitry Rodionov <d.rodionov@picodata.io>
2025-07-25 18:44:32 +0300

    ci: update clone script to work for tags

--- tools/clone.sh
+++ tools/clone.sh
@@ -2,26 +2,28 @@
 
 set -euxo pipefail
 
-# get start time
-start_time=$(date +%s)
+git config --global fetch.parallel 8
 
 # check if .git exist, fetch current commit and update submodules
 if [ -d "$CI_PROJECT_DIR/.git" ]; then
   cd $CI_PROJECT_DIR
   # parallel clone/fetch
-  git config fetch.parallel 8
   git clean -fdx
-  git branch -D $CI_COMMIT_REF_NAME || echo "$CI_COMMIT_REF_NAME doesn't exist"
-  git fetch -uf origin $CI_COMMIT_REF_NAME:$CI_COMMIT_REF_NAME
+
+  # remove cached version of local branch/tag
+  git branch -D $CI_COMMIT_REF_NAME || echo "branch $CI_COMMIT_REF_NAME doesn't exist"
+  git tag -d $CI_COMMIT_REF_NAME || echo "tag $CI_COMMIT_REF_NAME doesn't exist"
+
+  # fetch remote tags overwriting local ones (in case tag was removed from remote but still exists in cache)
+  git fetch --tags --force
+
+  git fetch origin $CI_COMMIT_REF_NAME
   git checkout $CI_COMMIT_REF_NAME --force
   git submodule update --init --recursive
 else
   # do full clone
-  git clone --recursive -b $CI_COMMIT_REF_NAME -j8 https://git.picodata.io/${CI_PROJECT_PATH}.git $CI_PROJECT_DIR
+  git clone --recursive -b $CI_COMMIT_REF_NAME https://git.picodata.io/${CI_PROJECT_PATH}.git $CI_PROJECT_DIR
 fi
 
-# get end time and difference
-date
-end_time=$(date +%s)
-difference=$((end_time - start_time))
-echo "Clone took: $difference seconds"
+# report what we ended up with
+git rev-parse HEAD
