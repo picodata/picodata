21a8e4e0c9f39224e2929c7bb7f261d422a650c1
Dmitry Rodionov <d.rodionov@picodata.io>
2025-07-29 20:14:53 +0300

    ci: more tweaks to clone.sh script
    Version of the script tweaked to work with tags broke for non tag
    pipelines. Switch to direct fetching of the commit that triggered the
    pipeline. This should work equally well for tags and usual branches.

--- tools/clone.sh
+++ tools/clone.sh
@@ -2,23 +2,31 @@
 
 set -euxo pipefail
 
+# parallel clone/fetch
 git config --global fetch.parallel 8
 
 # check if .git exist, fetch current commit and update submodules
 if [ -d "$CI_PROJECT_DIR/.git" ]; then
   cd $CI_PROJECT_DIR
-  # parallel clone/fetch
+
+  git rev-parse --abbrev-ref HEAD
+
   git clean -fdx
 
-  # remove cached version of local branch/tag
-  git branch -D $CI_COMMIT_REF_NAME || echo "branch $CI_COMMIT_REF_NAME doesn't exist"
-  git tag -d $CI_COMMIT_REF_NAME || echo "tag $CI_COMMIT_REF_NAME doesn't exist"
-
-  # fetch remote tags overwriting local ones (in case tag was removed from remote but still exists in cache)
+  # fetch remote tags overwriting local ones
+  # important in case tag was removed from remote but still exists in cache
+  # (typical case when release didnt go smoothly and we want to change release tag)
   git fetch --tags --force
 
+  # fetch fresh version of branch/tag
   git fetch origin $CI_COMMIT_REF_NAME
-  git checkout $CI_COMMIT_REF_NAME --force
+
+  # do checkout by commit, this works for both, regular branches and tags
+  # note that there can be some weird cases, i e when cached git directory
+  # was created from a branch and then pipeline triggered for force-pushed
+  # version of that branch, so tip of the branch from the cache is no longer valid
+  git checkout $CI_COMMIT_SHA --force
+
   git submodule update --init --recursive
 else
   # do full clone
