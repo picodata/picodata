a6cdea80a5f0908df2efd064c3aae7351801ef72
Georgy Moshkin <gmoshkin@picodata.io>
2025-10-14 15:35:26 +0300

    feat: add support to inject errors with payload
    Usage:
    
    Pass the payload as a string
    
    1) Using environment variables like this:
    
        PICODATA_ERROR_INJECTION_MY_INJECTION="arbitrary text"
    
    2) Or via lua function
    
        pico._inject_error("MY_INJECTION", "arbitrary text")
    
    In source code access the payload using the macro
    
        crate::error_injection!("MY_INJECTION" => |value| {
            crate::tlog!(Info, "injected error: {value}")
        });
    
    (cherry picked from commit b692c40e27239aafcdc708fb5433eb496bc4670e)

--- src/error_injection.rs
+++ src/error_injection.rs
@@ -1,48 +1,58 @@
 #[allow(unused_imports)]
-use std::{collections::HashSet, ptr};
+use std::{collections::HashMap, ptr};
 
 #[cfg(feature = "error_injection")]
-static mut INJECTED_ERRORS: Option<HashSet<String>> = None;
+static mut INJECTED_ERRORS: Option<HashMap<String, String>> = None;
 
 #[cfg(feature = "error_injection")]
 #[inline(always)]
-pub fn enable(error: &str, enable: bool) {
+pub fn enable(error: &str, value: impl Into<String>) {
     // SAFETY: safe as long as only called from tx thread
     let injected_errors =
         unsafe { crate::static_ref!(mut INJECTED_ERRORS).get_or_insert_default() };
 
     // DO NOT LOG "ERROR INJECTION '{error}'" HERE!!!
     // We check this error message in tests to catch the moment the injected error happens
-    if enable {
-        injected_errors.insert(error.into());
-    } else {
-        injected_errors.remove(error);
-    }
+    injected_errors.insert(error.into(), value.into());
+}
+
+#[cfg(feature = "error_injection")]
+pub fn disable(error: &str) {
+    // SAFETY: safe as long as only called from tx thread
+    let injected_errors =
+        unsafe { crate::static_ref!(mut INJECTED_ERRORS).get_or_insert_default() };
+
+    injected_errors.remove(error);
 }
 
 #[inline(always)]
 pub fn is_enabled(error: &str) -> bool {
+    injected_value(error).is_some()
+}
+
+#[inline(always)]
+pub fn injected_value(error: &str) -> Option<&str> {
     // SAFETY: safe as long as only called from tx thread
     #[cfg(feature = "error_injection")]
     if let Some(injected_errors) = unsafe { crate::static_ref!(const INJECTED_ERRORS) } {
-        return injected_errors.contains(error);
+        return injected_errors.get(error).map(String::as_ref);
     }
 
     _ = error;
-    false
+    None
 }
 
 // Scan environment variables for `PICODATA_ERROR_INJECTION_<NAME>`-like
 // patterns. Arm corresponding injection for each one.
 #[cfg(feature = "error_injection")]
 pub fn set_from_env() {
-    for (key, _) in std::env::vars() {
+    for (key, value) in std::env::vars() {
         let inject_name = match key.strip_prefix("PICODATA_ERROR_INJECTION_") {
             Some(name) => name,
             None => continue,
         };
 
-        enable(inject_name, true)
+        enable(inject_name, value)
     }
 }
 
@@ -94,12 +104,13 @@ macro_rules! error_injection {
             return result;
         }
     }};
-    ($error:expr => $block:block) => {{
+    ($error:expr => $(| $value:ident |)? $block:block) => {{
         let error = $error;
-        if $crate::error_injection::is_enabled(error) {
+        if let Some(_value) = $crate::error_injection::injected_value(error) {
             $crate::tlog!(Info, "################################################################");
             $crate::tlog!(Info, "ERROR INJECTION '{error}': EXECUTING");
             $crate::tlog!(Info, "################################################################");
+            $( let $value = _value; )?
             $block;
         }
     }}
