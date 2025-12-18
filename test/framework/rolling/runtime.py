8ae84bfa3fc705359104e289b251aac3a2a5c838
Kirill Bezuglyi <k.bezuglyi@picodata.io>
2025-12-18 16:49:37 +0300

    test(rolling): raise explicit error for tests against zero patch versions
    In our current workflow, versions that contain zero component (e.g.
    the first version after minor release will contain a zero in PATCH) are
    considered not valid versions, because they are used only to mark
    beginning of a new release series, so there are no packages built for them.
    This causes rolling upgrade tests to fail, because they rely on packages
    for Rocky Linux to use in CI. Particular case that we encountered
    includes new minor 25.5 being branched without 25.5.1 tag yet. So in
    master we need to start new release series, 25.6 which implies tagging
    25.6.0. So we have 25.6.0 which attempts to test against 25.5.0 because
    there is no 25.5.1. The solution to that is to always tag a new release
    immediately at branch point. So there is no such problem.
    
    This commit adds explicit error for that case.
    
    Co-Authored-By: Dmitry Rodionov <d.rodionov@picodata.io>

--- test/framework/rolling/runtime.py
+++ test/framework/rolling/runtime.py
@@ -13,11 +13,33 @@ from framework.rolling.version import RelativeVersion
 import pytest
 
 
-@dataclass
+@dataclass(init=False)
 class Runtime:
     absolute_version: AbsoluteVersion
     relative_version: RelativeVersion
-    runner_entity: Optional[Path] = None
+    runner_entity: Optional[Path]
+
+    def __init__(
+        self,
+        absolute_version: AbsoluteVersion,
+        relative_version: RelativeVersion,
+        runner_entity: Optional[Path] = None,
+    ) -> None:
+        if absolute_version.micro == 0 and relative_version != RelativeVersion.CURRENT:
+            # NOTE: this problem occurs when we branched new minor, but didnt make a release yet.
+            # So e g we have 25.5.0 tag from previous series without 25.5.1 tag and 25.6.0 tag in master.
+            # For 25.6.0 we cant really find previous minor, which would've been 25.5.1 but 25.5.1 is not tagged yet.
+            # We attempted a solution with rc tag e g 25.5.1-rc1 but it doesnt work with our packaging solution -
+            # packpack fails to parse such versions.
+            reason = f"Cannot test against {relative_version.name} version \
+                ({absolute_version}) as it has a zero component. \
+                We dont build packages for such versions. \
+                The test will work once non-zero {absolute_version} release is tagged."
+            ValueError(reason)
+
+        self.absolute_version = absolute_version
+        self.relative_version = relative_version
+        self.runner_entity = runner_entity
 
     @classmethod
     def current(cls) -> Runtime:
