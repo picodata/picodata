66db16db8f52bad5006416048faa28766c1ef481
Georgy Moshkin <gmoshkin@picodata.io>
2026-02-11 14:13:41 +0300

    test: add a test for expel during unfinished upgrade

--- test/int/test_rolling.py
+++ test/int/test_rolling.py
@@ -484,3 +484,69 @@ cluster:
         assert base_version(response["currentInstaceVersion"]) == str(runtime_v2.absolute_version)
         # but currentVersion is the same
         assert base_version(response["clusterVersion"]) == str(runtime_v2.absolute_version)
+
+
+@pytest.mark.xdist_group(name="rolling")
+def test_expel_working_during_upgrade(
+    registry: Registry,
+    cluster: Cluster,
+):
+    cluster.registry = registry
+    cluster.set_config_file(
+        yaml="""
+cluster:
+    name: test
+    tier:
+        arbiter:
+            can_vote: true
+        storage:
+            can_vote: false
+            replication_factor: 2
+        """
+    )
+
+    runtime_v1 = cluster.registry.get(Version.PREVIOUS_MINOR)
+    cluster.runtime = runtime_v1
+    leader = cluster.add_instance(wait_online=False, name="leader", tier="arbiter")
+    storage_A = cluster.add_instance(wait_online=False, name="storage_A", tier="storage")
+    storage_B = cluster.add_instance(wait_online=False, name="storage_B", tier="storage")
+    cluster.wait_online()
+
+    # Initial cluster version
+    [[cluster_version]] = leader.sql("SELECT value FROM _pico_property WHERE key = 'cluster_version'")
+    assert base_version(cluster_version) == str(runtime_v1.absolute_version)
+
+    # Change auto-offline timeout to check senitnel's behaviour later
+    leader.sql("ALTER SYSTEM SET governor_auto_offline_timeout = 3")
+
+    # Turn off one of the instances to block upgrade
+    storage_B.terminate()
+    leader.wait_has_states("Offline", "Offline", target=storage_B)
+
+    # Start upgrading instances one by one
+    runtime_v2 = cluster.registry.get(Version.CURRENT)
+
+    storage_A.terminate()
+    storage_A.runtime = runtime_v2
+    storage_A.start()
+    storage_A.wait_online()
+
+    leader.terminate()
+    leader.runtime = runtime_v2
+    leader.start()
+    leader.wait_online()
+
+    # Cluster wasn't upgraded yet, because `storage_B` is not online
+    [[cluster_version]] = leader.sql("SELECT value FROM _pico_property WHERE key = 'cluster_version'")
+    assert base_version(cluster_version) == str(runtime_v1.absolute_version)
+
+    # Expel the last instance which is currently blocking the upgrade
+    cluster.expel(target=storage_B)
+
+    # Now cluster is successfully upgraded
+    def check():
+        [[cluster_version]] = leader.sql("SELECT value FROM _pico_property WHERE key = 'cluster_version'")
+        assert base_version(cluster_version) == str(runtime_v2.absolute_version)
+
+    # Note: needs to be retriable because upgrade happens asynchronously after all instances become Online
+    Retriable().call(check)
