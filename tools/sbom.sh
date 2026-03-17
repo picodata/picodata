f27016299ca1f8fd774abb6da46985622537be41
Dmitry Rodionov <d.rodionov@picodata.io>
2026-03-17 11:00:35 +0300

    chore: tweak sbom infra to fix webui sbom and extend checks
    Some other minor tweaks including proper attributes for openssl so
    `sbom-to-odt.py` can properly infer source language.
    
    (cherry picked from commit 18ea1863fcb692ed8907e7f13b5c707d8e45b367)

--- tools/sbom.sh
+++ tools/sbom.sh
@@ -10,15 +10,33 @@ SBOM_RUST=${SBOM_RUST:-1}
 SBOM_JS=${SBOM_JS:-1}
 
 
+# Not a submodule because we have github mirror, so we would have to mirror this repo there as well for
+# --recurse-submodules to work during git clone
+if [ ! -d "$SBOM_CHECKER_DIR" ]; then
+    git clone git@git.picodata.io:core/sbom-checker.git "$SBOM_CHECKER_DIR"
+    if [ -z "${VIRTUAL_ENV:-}" ]; then
+        python3 -m venv "$SBOM_CHECKER_DIR/venv"
+        . "$SBOM_CHECKER_DIR/venv/bin/activate"
+        pip install -r "$SBOM_CHECKER_DIR/requirements.txt"
+    fi
+fi
+
 if [ "$SBOM_JS" == "1" ]; then
     # build separate SBOM file for webui
     # see "sbom" script in package.json
     # we use a full
     pushd webui
     yarn install --immutable
-    yarn cyclonedx --prod --gather-license-texts --output-reproducible -o ../webui_sbom.json
+    yarn cyclonedx --prod --gather-license-texts --output-reproducible -o ../webui.cdx.json
     popd
+
+    # We do not pass attack_surface and security_function files because for webui there are no
+    # security sensitive components, so script sets default "no" value for all components
+    python3 "$SBOM_CHECKER_DIR"/picodata_sbom_tools.py \
+        --manufacturer "ООО Пикодата" \
+        "$PROJECT_DIR"/webui.cdx.json "$PROJECT_DIR"/webui-fixed.cdx.json
 fi
+
 
 if [ "$SBOM_RUST" != "1" ]; then
     # all the lines below are rust sbom building
@@ -35,17 +53,6 @@ cargo install --quiet --locked \
 cargo cyclonedx --format json --spec-version 1.6
 
 
-# Not a submodule because we have github mirror, so we would have to mirror this repo there as well for
-# --recurse-submodules to work during git clone
-if [ ! -d "$SBOM_CHECKER_DIR" ]; then
-    git clone git@git.picodata.io:core/sbom-checker.git "$SBOM_CHECKER_DIR"
-    if [ -z "${VIRTUAL_ENV:-}" ]; then
-        python3 -m venv "$SBOM_CHECKER_DIR/venv"
-        . "$SBOM_CHECKER_DIR/venv/bin/activate"
-        pip install -r "$SBOM_CHECKER_DIR/requirements.txt"
-    fi
-fi
-
 python3 "$SBOM_CHECKER_DIR"/picodata_sbom_tools.py \
     --manufacturer "ООО Пикодата" \
     --external-references-map-file "$SBOM_METADATA_DIR"/external_references.json \
@@ -54,10 +61,14 @@ python3 "$SBOM_CHECKER_DIR"/picodata_sbom_tools.py \
     --extra-components "$SBOM_METADATA_DIR"/extra_components.json \
     "$PROJECT_DIR"/picodata.cdx.json "$PROJECT_DIR"/picodata-fixed.cdx.json
 
+
 # sbom-checker requires python >= 3.8. not all our supported OS have such version
 if [ "$SBOM_VERIFY" == "1" ]; then
     if [ -z "${VIRTUAL_ENV:-}" -a -f "$SBOM_CHECKER_DIR/venv/bin/activate" ]; then
     . "$SBOM_CHECKER_DIR/venv/bin/activate"
     fi
-    python3 "$SBOM_CHECKER_DIR"/sbom-checker.py -v "$PROJECT_DIR"/picodata-fixed.cdx.json
+    python3 "$SBOM_CHECKER_DIR"/sbom-checker.py --check-vcs -v "$PROJECT_DIR"/picodata-fixed.cdx.json
+    python3 "$SBOM_CHECKER_DIR"/sbom-checker.py --check-source-distribution -v "$PROJECT_DIR"/picodata-fixed.cdx.json
+    python3 "$SBOM_CHECKER_DIR"/sbom-checker.py --check-vcs -v "$PROJECT_DIR"/webui-fixed.cdx.json
+    python3 "$SBOM_CHECKER_DIR"/sbom-checker.py --check-source-distribution -v "$PROJECT_DIR"/webui-fixed.cdx.json
 fi
