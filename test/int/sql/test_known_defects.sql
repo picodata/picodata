0acc62c5884bb882c2e55a9473973b1e805bb9d1
Dmitry Ivanov <ivadmi5@gmail.com>
2026-03-19 13:41:39 +0300

    fix(test): rewrite test_known_defects in terms of decimal
    According to
    
    > tuple: fix confusing hashing of 'number' index
    
    the hash function for both float and double now treats all
    integral values as if they were in fact integers.
    
    Since the test has never been about `double` in particular,
    let's replace `double` with `decimal` to illustrate the
    problem.

--- test/int/sql/test_known_defects.sql
+++ test/int/sql/test_known_defects.sql
@@ -1,14 +1,14 @@
 -- TEST: invalid-bucket-calculation-gl2640
 -- SQL:
-CREATE TABLE t (a INT, b DOUBLE, PRIMARY KEY (b));
+CREATE TABLE t (a INT, b DECIMAL, PRIMARY KEY (b));
 
 
 -- TEST: invalid-bucket-calculation-gl2640.2
 -- SQL:
 EXPLAIN SELECT * FROM t WHERE b = 1;
 -- EXPECTED:
-projection ("t"."a"::int -> "a", "t"."b"::double -> "b")
-    selection "t"."b"::double = 1::int
+projection ("t"."a"::int -> "a", "t"."b"::decimal -> "b")
+    selection "t"."b"::decimal = 1::int
         scan "t"
 execution options:
     sql_vdbe_opcode_max = 45000
@@ -17,12 +17,50 @@ buckets = [1934]
 
 -- TEST: invalid-bucket-calculation-gl2640.3
 -- SQL:
-EXPLAIN SELECT * FROM t WHERE b = 1::double;
+EXPLAIN SELECT * FROM t WHERE b = 1::decimal;
 -- EXPECTED:
-projection ("t"."a"::int -> "a", "t"."b"::double -> "b")
-    selection "t"."b"::double = 1::double
+projection ("t"."a"::int -> "a", "t"."b"::decimal -> "b")
+    selection "t"."b"::decimal = 1::decimal
         scan "t"
 execution options:
     sql_vdbe_opcode_max = 45000
     sql_motion_row_max = 5000
-buckets = [2099]
+buckets = [2135]
+
+-- TEST: invalid-bucket-calculation-gl2640.4
+-- SQL:
+EXPLAIN SELECT * FROM t WHERE b = 1.0;
+-- EXPECTED:
+projection ("t"."a"::int -> "a", "t"."b"::decimal -> "b")
+    selection "t"."b"::decimal = 1.0::decimal
+        scan "t"
+execution options:
+    sql_vdbe_opcode_max = 45000
+    sql_motion_row_max = 5000
+buckets = [712]
+
+-- TEST: invalid-bucket-calculation-gl2640.5
+-- SQL:
+EXPLAIN INSERT INTO t VALUES (1, 1);
+-- EXPECTED:
+insert "t" on conflict: fail
+    motion [policy: segment([ref("COLUMN_2")]), program: ReshardIfNeeded]
+        values
+            value row (data=ROW(1::int, 1::int))
+execution options:
+    sql_vdbe_opcode_max = 45000
+    sql_motion_row_max = 5000
+buckets = [1934]
+
+-- TEST: invalid-bucket-calculation-gl2640.6
+-- SQL:
+EXPLAIN INSERT INTO t VALUES (1, 1.0);
+-- EXPECTED:
+insert "t" on conflict: fail
+    motion [policy: segment([ref("COLUMN_2")]), program: ReshardIfNeeded]
+        values
+            value row (data=ROW(1::int, 1.0::decimal))
+execution options:
+    sql_vdbe_opcode_max = 45000
+    sql_motion_row_max = 5000
+buckets = [712]
