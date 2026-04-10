3a33d7067d053067584d116e51393dca1608bf5b
Kirill Bezuglyi <k.bezuglyi@picodata.io>
2026-04-10 13:00:17 +0300

    test(rolling): wait for governor to finish operations after cluster upgrade
    After changing the executable of every instance in a cluster, the
    governor may still be processing the upgrade. Returning from
    `Cluster.change_executable` before it finishes can cause subsequent
    checks to race against in-flight governor operations.
    
    Wait for the governor to reach the "idle" state before returning, so
    callers can safely assert post-upgrade invariants.
    
    Part-of: picodata!2502
    See-also: picodata!2853
    (cherry picked from commit 038213ed9ea64eba2eacbf0490e1ac8d4facd098)

--- test/conftest.py
+++ test/conftest.py
@@ -2921,6 +2921,7 @@ class Cluster:
     ):
         for instance in self.instances:
             instance.change_executable(to, error)
+        self.leader().wait_governor_status("idle")
 
     def pick_random_instance(
         self,
