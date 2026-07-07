c658b603cac8b42603bf9f7ec7144a87f912f275
Nikita Strygin <n.strygin@picodata.io>
2026-07-07 19:25:08 +0300

    fix(test): remove `test_restart_timing`
    The test originally intended to test that an improvement to instance
    restart time produced by sending `proc_update_instance` RPCs existed.
    
    However, measuring a performance metric as part of a regular test suite
    proved to be unreliable and the test has flaked. It has already proven
    its point (the improvement is there), so it's better to remove it for
    the sake of more stable CI.

--- test/int/test_restart.py
+++ test/int/test_restart.py
@@ -1,53 +1,7 @@
 import threading
-import time
 
 import pytest
 from conftest import Cluster, TarantoolError, log_crawler
-from framework.log import log
-
-
-def test_restart_timing(cluster: Cluster):
-    """
-    Test that checks timings of picodata restart.
-    Proactive sending of `proc_update_instance` during `postjoin` should make it fast & predictable.
-    """
-    cluster.set_config_file(
-        yaml="""
-cluster:
-    name: test
-    tier:
-        arbiter:
-            can_vote: true
-        storage:
-            can_vote: false
-"""
-    )
-
-    _arbiter1 = cluster.add_instance(tier="arbiter", wait_online=False)
-    _arbiter2 = cluster.add_instance(tier="arbiter", wait_online=False)
-    _arbiter3 = cluster.add_instance(tier="arbiter", wait_online=False)
-    storage = cluster.add_instance(tier="storage", wait_online=False)
-    cluster.wait_online()
-
-    cluster.wait_leader_elected()
-
-    # now restart the storage node 10 times and see how long each restart takes
-    for _ in range(10):
-        storage.terminate()
-
-        start = time.time()
-
-        storage.start()
-        storage.wait_online()
-
-        elapsed = time.time() - start
-
-        log.info(f"Restart completed in {elapsed} seconds")
-
-        # In my testing, after implementation of proactive `proc_update_instance`, `elapsed` is consistently sitting at 2.01 seconds
-        # While before enabling proactive `proc_update_instance` it sits around 4-6 seconds
-        # Use a slightly higher threshold to reduce flaky false positives
-        assert elapsed < 5
 
 
 def test_wait_vshard_storage(cluster: Cluster):
