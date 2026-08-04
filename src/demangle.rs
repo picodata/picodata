721343079d962e0947064d6a0dee16be3635a60c
Dmitry Ivanov <ivadmi5@gmail.com>
2026-08-04 16:27:10 +0300

    test: add tests for symbol demangler module

--- src/demangle.rs
+++ src/demangle.rs
@@ -34,3 +34,48 @@ extern "C" fn tnt_abi_demangle(mangled_name: *const c_char) -> *const c_char {
         guard.as_ptr() as _
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        ffi::{CStr, CString},
+        str::FromStr,
+    };
+
+    fn try_demangle(name: &str) -> String {
+        let name = CString::from_str(name).unwrap();
+        let ptr = tnt_abi_demangle(name.as_ptr());
+        unsafe { CStr::from_ptr(ptr) }.to_str().unwrap().to_owned()
+    }
+
+    #[test]
+    fn check_demangle() {
+        let res = try_demangle("");
+        assert_eq!(res, "");
+
+        let large_input: String = std::iter::repeat_n('a', 16384).collect();
+        let res = try_demangle(&large_input);
+        assert!(large_input.starts_with(res.as_str()));
+
+        // C
+        let res = try_demangle("foobar");
+        insta::assert_snapshot!(res, @"foobar");
+
+        // C++
+        let res = try_demangle("_ZZN3icu13ICUServiceKey16getStaticClassIDEvE7classID");
+        insta::assert_snapshot!(res, @"icu::ICUServiceKey::getStaticClassID()::classID");
+
+        // C++
+        let res = try_demangle("_ZZN3icu12_GLOBAL__N_113AliasReplacer7replaceERKNS_6LocaleERNS_10CharStringER10UErrorCodeENUlPvE_4_FUNES9_");
+        insta::assert_snapshot!(res, @"icu::(anonymous namespace)::AliasReplacer::replace(icu::Locale const&, icu::CharString&, UErrorCode&)::{lambda(void*)#1}::_FUN(void*)");
+
+        // Rust
+        let res = try_demangle("_RNvXsq_NtNtCsJjg3s9kj1t_8picodata7pgproto5errorNtB5_13PedanticErrorNtNtCscI6d9CVNmLh_4core3fmt7Display3fmt");
+        insta::assert_snapshot!(res, @"<picodata::pgproto::error::PedanticError as core::fmt::Display>::fmt");
+
+        // Rust
+        let res = try_demangle("_RNvXsp_NtCscI6d9CVNmLh_4core6resultINtB5_6ResultTINtNtNtCsJjg3s9kj1t_8picodata7pgproto6stream8PgStreamNtNtCs5QvSrEf57G0_9tarantool4coio10CoIOStreamENtNtNtBQ_6client7startup12ClientParamsENtNtBQ_5error7PgErrorENtNtNtB7_3ops9try_trait3Try6branchBS_");
+        insta::assert_snapshot!(res, @"<core::result::Result<(picodata::pgproto::stream::PgStream<tarantool::coio::CoIOStream>, picodata::pgproto::client::startup::ClientParams), picodata::pgproto::error::PgError> as core::ops::try_trait::Try>::branch");
+    }
+}
